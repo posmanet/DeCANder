ab1c1b1a30bb7f29144eaaaa3ed0edbf601d4a0e
Posmanet <posmanet@web.de>
2019-04-28 15:07:14 +0200

    first data interpreting and displaying version
    This version (2.0) is able to interpret the first two values (vehicle speed and enginge rpm) from the CAN-bus data and display them on LCD. Future versions will be able to select multiple displays (variable "modes") with the UP/DOWN keys on the shield.

--- DeCANder/DeCANder.ino
+++ DeCANder/DeCANder.ino
@@ -1,173 +1,113 @@
 /*
- * using LiquidCrystal Library (16x2 LCD display; Hitachi HD44780) <- LCD-keypad-shield
- * -> Arduino-Pins D4-D9 (and D10!!!) plus A0 for the keys
- * D10 = Backlight control!! DISCONNECT PIN 10 FROM LCD-SHIELD!
- * LCD RS pin to digital pin 8
- * LCD Enable pin to digital pin 9
- * LCD D4 pin to digital pin 4
- * LCD D5 pin to digital pin 5
- * LCD D6 pin to digital pin 6
- * LCD D7 pin to digital pin 7
- * LCD R/W pin to ground
- * 10K resistor: ends to +5V and ground; wiper to LCD VO pin (pin 3)
-*/
-
-/*
- * CAN-BUS Shield; DIGITAL V1.2 10/10/2013 by ElecFreaks (.com)
- * CAN V2.0 up to 1 Mb/s; feat. MCP2515 CAN Bus controller (SPI 10 MHz) & MCP2551 CAN transceiver
- * ODB-II converter cable (import ODB-II library 1.2 2014-07-08)
- * sub-D 2 -> GND -> CAN 4+5
- * sub-D 3 -> CAN H -> CAN 6
- * sub-D 5 -> CAN L -> CAN 14
- * sub-D 9 -> 12V -> CAN 16
- * Ardunino pin 13, 12, 11, 10(cs) and 2 are occupied ! Jackpot!!
+ * DeCANder: Landrover Defender TD4 / TDCI - Arduino UNO - CAN-bus display/tool
+ * display for additional information from the CAN-bus of a Land Rover Defender with the Ford "TD4" / "TDCI" / "duratorq" engine
+ * Created 2019 by Posmanet (Martin) and released on github.com.
+ * USE THIS CODE ON YOUR OWN RISK!
  */
 
-#include <LiquidCrystal.h>
-LiquidCrystal lcd(8, 9, 4, 5, 6, 7);
-int taster = 0;                                        // ADC button value from LCD-keypad-shield
-unsigned char canLen = 0;
-unsigned char canBuf[8];
-int IDs[] = {907, 297, 906, 1218, 246, 298, 296, 295, 1029, 1090, 1024}; // sorted by occurrence
-int ID = 0;
-int IDbyte = 0;
-int sel = 1;
-int warte = 250;
-int offset = 0;
-int twoByte = 0;
+// Constants
+#define DeCANderVer "2.0"
+#define modes 1                                             // number of available display modes
 
-#include <SPI.h>
-#include "mcp_can.h"
+// Variables
+int taster = 0;                                             // ADC button value from LCD-keypad-shield
+unsigned char canLen = 0;                                   // CAN
+unsigned char canBuf[8];                                    // CAN
+unsigned long canID = 0;                                    // CAN
+int warte = 50;                                             // keypress delay
+int mode = 1;                                               // selectable display mode (up/down)
+unsigned long lastmillis = 0;                               // display refresh timer
+float spd = 0;                                              // interpreted vehicle speed
+int rpm = 0;                                                // interpreted engine rpm
 
-MCP_CAN CAN(10);                                    // Set CS pin to Arduino pin 10
+
+// Libraries
+#include <LiquidCrystal.h>                                  // "LCD-keypad-shield from D1 Robot" using a 2x16 Hitachi HD44780 display
+LiquidCrystal lcd(8, 9, 4, 5, 6, 7);                        // Disconnect pin 10! It conflicts with the CAN-shield!
+#include <SPI.h>                                            // sending serial data via Arduinos USB-Port
+#include "mcp_can.h"                                        // CAN-bus-shield "DIGITAL V1.2 10/10/2013 by ElecFreaks"
+MCP_CAN CAN(10);                                            // Set CS pin to Arduino pin 10 - is hardwired on my shield!
 
 void setup() {
   Serial.begin(115200);
-  Serial.println("DeCANder v1.0");
+  Serial.print("DeCANder v");
+  Serial.println(DeCANderVer);
   Serial.println("=============");
-  Serial.println("Trying to connect to car's CAN system. Watch the LCD!");
-  
-  lcd.begin(16, 2);                                    // set up the LCD's number of columns and rows: 
-  lcd.print("DeCANder v1.0");
-  delay(1000);
-
-  while (CAN_OK != CAN.begin(CAN_500KBPS))              // init can bus : baudrate TD4 = 500k
-    {
-      lcd.setCursor(0,1);
-      lcd.print("CAN init fail...");
-      delay(1000);
-    }
-  lcd.setCursor(0,1);
-  lcd.print("CAN init ok     ");
-  delay(1000);
-  lcd.setCursor(0,0);
-  lcd.print("                ");
-  lcd.setCursor(0,1);
-  lcd.print("                ");
-}
-
-void showID() {
-  lcd.setCursor(0,0);
-  lcd.print("ID:");
-  if (IDs[ID] < 256) { lcd.print(" "); }
-  lcd.print(IDs[ID],HEX);
-  lcd.print(".");
-  lcd.print(IDbyte);
-  if (sel == 2) {
-    lcd.print("&");
-    if (IDbyte == 7) { lcd.print("0"); } else { lcd.print(IDbyte+1); }
-  } else {
-    lcd.print("  ");
+  Serial.println("connecting...");
+  lcd.begin(16, 2);                                         // set up the LCD's number of columns and rows: 
+  lcd.print(" DeCANder  v");
+  lcd.print(DeCANderVer);
+  delay(2000);
+  while (CAN_OK != CAN.begin(CAN_500KBPS)) {                // init can bus : baudrate TD4 = 500k
+    lcd.setCursor(0,1);
+    lcd.print(" CAN init fail. ");
+    delay(1000);
   }
+  lcd.setCursor(0,1);
+  lcd.print(" CAN init done! ");
+  delay(1000);
+  lcd.clear();
 }
 
-void showData() {
+void evalCAN() {
+  if (canID == 907)  { spd = (canBuf[4] * 256 + canBuf[5]) / 100; }
+  if (canID == 246)  { rpm = (canBuf[4] - 96) * 256 + canBuf[5]; }
+}
+
+void showMode1() {
+  lcd.setCursor(0,0);
+  lcd.print(" v : ");
+  if (spd < 10) { lcd.print("  "); } else if (spd < 100) { lcd.print(" "); }
+  lcd.print(spd,1);
+  lcd.print(" km/h ");
   lcd.setCursor(0,1);
-  if (canBuf[IDbyte] < 10) { lcd.print("  "); } else if (canBuf[IDbyte] < 100) { lcd.print(" "); }
-  lcd.print(canBuf[IDbyte]);
-  if (sel == 2 ) {
-    lcd.print(" ");
-    if (IDbyte == 7) {
-      if (canBuf[0] < 10) { lcd.print("  "); } else if (canBuf[0] < 100) { lcd.print(" "); }
-      lcd.print(canBuf[0]);
-    } else {
-      if (canBuf[IDbyte+1] < 10) { lcd.print("  "); } else if (canBuf[IDbyte+1] < 100) { lcd.print(" "); }
-      lcd.print(canBuf[IDbyte+1]);
-    }
-    lcd.print(" ");
-    if ((ID == 0) and (IDbyte == 4)) { offset = 0; } else
-    if ((ID == 0) and (IDbyte == 6)) { offset = 0; } else
-    if ((ID == 1) and (IDbyte == 6)) { offset = 12; } else
-    if ((ID == 2) and (IDbyte == 4)) { offset = 0; } else
-    if ((ID == 2) and (IDbyte == 6)) { offset = 0; } else
-    if ((ID == 4) and (IDbyte == 4)) { offset = 96; } else
-    if ((ID == 4) and (IDbyte == 6)) { offset = 0; } else
-    if ((ID == 6) and (IDbyte == 0)) { offset = 6; } else
-    if ((ID == 6) and (IDbyte == 2)) { offset = 7; } else
-    if ((ID == 6) and (IDbyte == 4)) { offset = 55; } else
-    if ((ID == 6) and (IDbyte == 6)) { offset = 7; } else { offset = 0; }
-    twoByte = (canBuf[IDbyte] - offset) * 256 + canBuf[IDbyte+1];
-    if (twoByte < 10) { lcd.print("    "); } else
-    if (twoByte < 100) { lcd.print("   "); } else
-    if (twoByte < 1000) { lcd.print("  "); } else
-    if (twoByte < 10000) { lcd.print(" "); }
-    lcd.print(twoByte);
-  } else {
-    lcd.print("             ");
+  lcd.print("rpm: ");
+  if (rpm <= 0) { lcd.print("engine off"); } else
+  if (rpm > 9999) { lcd.print(" off again"); } else {
+    if (rpm < 10) { lcd.print("   "); } else
+    if (rpm < 100) { lcd.print("  "); } else
+    if (rpm < 1000) { lcd.print(" "); }
+    lcd.print(rpm);
+    lcd.print(" 1/min ");
   }
 }
 
 void loop() {
-  lcd.setCursor(14, 0);
-  if ((millis()/1000%60) < 10) { lcd.print(" "); }
-  lcd.print(millis()/1000%60);
-
   taster = analogRead(A0);
-  if (taster < 71) { // r
-    if (sel == 2 ) {
-      if (IDbyte > 5) { IDbyte = 0; } else { IDbyte++; IDbyte++; }
-    } else {
-      if (IDbyte > 6) { IDbyte = 0; } else { IDbyte++; }
-     }
+  if (taster < 71) {                                        // r
     delay(warte);
-  } else if (taster < 235) { // u
-    if (ID < 1) { ID = 10; } else { ID--; }
+  } else if (taster < 235) {                                // u
+    if (mode == modes) { mode = 1; } else { mode++; }
+    lcd.clear();
     delay(warte);
-  } else if (taster < 416) { // d
-    if (ID == 10) { ID = 0; } else { ID++; }
+  } else if (taster < 416) {                                // d
+    if (mode == 1) { mode = modes; } else { mode--; }
+    lcd.clear();
     delay(warte);
-  } else if (taster < 624) { // l
-    if (sel == 2 ) {
-      if (IDbyte < 2) { IDbyte = 7; } else { IDbyte--; IDbyte--; }
-    } else {
-      if (IDbyte < 1) { IDbyte = 7; } else { IDbyte--; }
-     }
+  } else if (taster < 624) {                                // l
     delay(warte);
-  } else if (taster < 884) { // x
-    if (sel == 1) {
-      sel = 2;
-      if (IDbyte%2 > 0) { IDbyte--; };
-    } else {
-      sel = 1;
+  } else if (taster < 884) {                                // x
+    delay(warte);
+  }
+  if (CAN_MSGAVAIL == CAN.checkReceive()) {                 // check if data coming
+    CAN.readMsgBuf(&canLen,canBuf);                         // read data; len: data length, buf: data buf
+    canID = CAN.getCanId();
+    Serial.print(canID,HEX);
+    Serial.print(",");
+    for (int i = 0; i < canLen; i++) {                      // print data (serial)
+      Serial.print(canBuf[i],HEX);
+      if (i < (canLen - 1)) { Serial.print(","); }
     }
-    delay(warte);
-  }
-  showID();
-
-  
-  if (CAN_MSGAVAIL == CAN.checkReceive())            // check if data coming
-  {
-    CAN.readMsgBuf(&canLen, canBuf);    // read data,  len: data length, buf: data buf
-    unsigned long canID = CAN.getCanId();
-    Serial.println(canID, HEX);
-    Serial.println(",");
-    for (int i = 0; i < canLen; i++)    // print the data
-      {
-        Serial.print(canBuf[i], HEX);
-        if (i<(canLen-1)) { Serial.print(","); }
-      }
-    if (canID == IDs[ID]) { showData(); }
     Serial.println();
+    evalCAN();
   }
-
+  if (millis() > (lastmillis + 1000)) {
+         if (mode == 1) { showMode1(); }
+//    else if (mode == 2) { showMode2(); }
+//    else if (mode == 3) { showMode3(); }
+//    else if (mode == 4) { showMode4(); }
+//    else if (mode == 5) { showMode5(); }
+//    else { showMode6(); }
+    lastmillis = millis();
+  }
 }
--- README.md
+++ README.md
@@ -1,7 +1,9 @@
 # DeCANder
 Landrover Defender TD4 / TDCI - Arduino UNO - CAN-bus display/tool
 
-This is a small project for creating a heads-up-display for additional information from the CAN-bus of a Land Rover Defender with the Ford "TD4" / "TDCI" / "duratorq" engine. This 4x4 has a CAN-bus connection between the dashboard and the engine control unit (ECU).
+USE THIS CODE ON YOUR OWN RISK!
+
+This is a small project for creating a display for additional information from the CAN-bus of a Land Rover Defender with the Ford "TD4" / "TDCI" / "duratorq" engine. This 4x4 has a CAN-bus connection between the dashboard and the engine control unit (ECU).
 
 The idea is not to send messages to the CAN bus (i.e. standard OBD requests) but only to read and interpret the messages from the bus that are being sent from the car's own instances. These messages are not documented whatsoever. Thus this information has to be reverse-engineered for the CAN-bus traffic to make sense.
 
@@ -10,6 +12,10 @@ Most of the data already could be recognized - some other data is still a myster
 Hardware:
 I am using an Arduino "UNO" + a CAN-bus-shield "DIGITAL V1.2 10/10/2013 by ElecFreaks" + a "LCD-keypad-shield from D1 Robot" using a 2x16 Hitachi HD44780 display. Supply voltage +12V can be taken from the car's OBD connector. "CAN H", "CAN L", "+12V and "GND" have to be connected to the d-sub 9 connector on the CAN-bus-shield. PIN 10 from the display-shield conflicts with the CAN-bus-shield so i just desoldered this pin for now. On the LCD-keypad-shield it is used to dim the LCD backlight. Disconnected, the backlight is set to 100% - so no serious problem here.
 
-Software:
-This version is quite useful already - especially in the matter of reverse engineering capabilities. The CAN-bus is being connected and all messages read. All the messages are being brought to a csv-like shape and send serial via USB so you can collect data with a connected computer and terminal. (i.e. putty for MS-Windows or Linux) CAN-data-bytes can also be displayed on the LCD-keypad-shield. The CAN-ID and one of the 8 data bytes will be shown. You can select the CAN-ID by pressing UP/DOWN buttons and select the data byte by pressing LEFT/RIGHT buttons. By pressing the SELECT button two data bytes are being interpreted as a "hi" an "low" byte, being calculated as a two-byte-value and displayed.
+Libraries used:
+SPI.h for sending serial data via USB.
+LiquidCrystal.h for the LCD on the LCD-keypad-shield
+mcp_can.h for the CAN-bus-shield (download from manufacturer and save in libraries!)
 
+Software:
+This Arduino Code interprets and displays selected CAN-data on LCD and sends all CAN-data in a csv-like shape via USB so you can collect it with a connected computer and terminal. (i.e. putty for MS-Windows or Linux) Keys from the LCD-keypad-shield select display modes (UP/DOWN).
